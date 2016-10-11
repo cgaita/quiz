06663da0fb1ff15de360d4b61621ab262f66cd29
cgaita <gaitacaroline@gmail.com>
2016-10-11 19:23:47 -0400

    tracker done

--- app.js
+++ app.js
@@ -1,53 +1,42 @@
 /*
 ~1. list question one at a time
 ~2. pick answer
-~3. give feedback
-4. track question number
-5. no skipping
-6. only picking one answer
-7. give score at end
-8. retake
+~3. only select with button
+~4. give feedback
+~5. track question number
+6. no skipping
+~7. only picking one answer
+8.
+8. give score at end
+9. retake
 */
 $(document).ready(function(){
 	takeQuiz();
-	/*$('#start').click(function(){
-		$('#start_page').hide();
-		$('#questions_page').show();
-		displayQuestions();
-		$('#results').hide();
-		selectAnswer();
-	});
-	$('#next').click(function(){
-		$('.questions .text').html("");
-		$('.questions ul').html("");
-		changeQuestion();
-		selectAnswer();
-		});*/
 	
 });
 //objects 
 var question1 = {
-	text: "What is the cat’s name?",
+	text: "1. What is the cat’s name?",
 	answers: ["Midnight", "Zachery", "Shadow", "Thackery"],
 	correctAnswer: 3
 }
 var question2 = {
-	text: "What Sanderson sister is the first to die at the end of the film?",
+	text: "2. What Sanderson sister is the first to die at the end of the film?",
 	answers: ["Sarah", "Winnie", "Mary", "Alison"],
 	correctAnswer: 0
 }
 var question3 = {
-	text: "Who has to light the Black Flamed Candle?",
+	text: "3. Who has to light the Black Flamed Candle?",
 	answers: ["A witch", "A child", "A virgin", "Anyone"],
 	correctAnswer: 2
 }
 var question4 = {
-	text: "What famous house is shown in the background of the movie?",
+	text: "4. What famous house is shown in the background of the movie?",
 	answers: ["The Christmas Vacation house", "The Home Alone house", "The House from Casper", "The House from Beetlejuice"],
 	correctAnswer: 0
 }
 var question5 = {
-	text: "What month was Hocus Pocus released in?",
+	text: "5. What month was Hocus Pocus released in?",
 	answers: ["September", "July", "October", "August"],
 	correctAnswer: 1
 }
@@ -60,21 +49,13 @@ function displayQuestions(){
 	var answersText ="";
 	var answers = currentQuestion.answers;
 	for (var i=0; i <= answers.length -1; i++){
-		answersText=answersText + '<li class="answers '+ i +'"><button class="button '+ i +'"></button>' + answers[i] + '</li>'
+		answersText=answersText + '<input type="radio" name="questions" value="'+ i +'" class="'+ i +'">'+ answers[i] +'<br>';
 	}
-	$('ul').append(answersText);
+	$('form').append(answersText);
 }
-
-// shows results page after questions
-	$('#next').click(function(){
-		if (currentQuestionIndex === allQuestions.length -1){
-		$('#questions_page').hide();
-		$('#results').show();
-		};
-	});
 	
 function selectAnswer(){
-	$('.answers').click(function(e){
+	$('input[type="radio"]').click(function(e){
 		var feedback = $(e.target).hasClass(allQuestions[currentQuestionIndex].correctAnswer);
 		if (feedback === true){
 			$('.feedback').html("Correct Answer!");
@@ -89,13 +70,8 @@ function changeQuestion(){
 	currentQuestionIndex++;
 	displayQuestions();
 	$('.feedback').html("");
-	//track progress not working
-	/*
-		$('#progress').html(function(){
-		
-		});
+
 	
-	*/
 };
 function takeQuiz(){
 	$('#start').click(function(){
@@ -104,18 +80,30 @@ function takeQuiz(){
 		$('#results').hide();
 		displayQuestions();
 		selectAnswer();
+		//track progress not working
+		$('#progress').html((currentQuestionIndex+1)+"/"+allQuestions.length)
 	});
 	$('#next').click(function(){
 		$('.questions .text').html("");
-		$('.questions ul').html("");
-		changeQuestion();
-		selectAnswer();
-		});
+		$('.questions form').html("");
+			console.log(currentQuestionIndex, allQuestions.length);
+		if (currentQuestionIndex === allQuestions.length -1){
+
+			$('#questions_page').hide();
+			$('#results').show();
+		}
+		else {
+			changeQuestion();
+			$('#progress').html((currentQuestionIndex+1)+"/"+allQuestions.length)
+			selectAnswer();
+		};
+	
+	});
 };
-/* restart quiz set all back to 0
+/*restart quiz set all back to 0
 function retakeQuiz(){
 	$('#retake').click(function(){
-	
+		
 	});
 };*/
 
